856fb424d2cd4602fcceee7e668dc50f1b9fa11e
nguyenthuy163 <thuyle.31231026554@st.ueh.edu.vn>
2024-10-22 20:52:13 +0700

    push them bai 3

---
+++ CSDL-Exercises-LeDangNguyenThuy/Exercise-Session3.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeDangNguyenThuy31231026554
+{
+    internal class Exercise_session3
+    {
+        public static void Main()
+        {
+            //Question02();
+            //Question03();
+            Question05();
+
+            Console.ReadKey();
+        }
+        public static void Question02()
+        {
+            Console.Write("Enter an integer for y from -5 to 5: ");
+            string input = Console.ReadLine();
+            int y;
+            if (int.TryParse(input, out y))
+            {
+                if (y >= -5 && y <= 5)
+                {
+                    int x = y * y + 2 * y + 1;
+                    Console.WriteLine($"y= {y}, x= {x}");
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a value from -5 to 5");
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Please enter an integer");
+            }
+
+        }
+        public static void Question03()
+        {
+            Console.Write("Enter the distance (in kilometers): ");
+            double distance = double.Parse(Console.ReadLine());
+            Console.Write("Enter the time - hours: ");
+            int hours = int.Parse(Console.ReadLine());
+            Console.Write("Enter the time - minutes: ");
+            int minutes = int.Parse(Console.ReadLine());
+            Console.Write("Enter the time - seconds: ");
+            int seconds = int.Parse(Console.ReadLine());
+            double totaltimeinhours = hours + (minutes / 60.0) + (seconds / 360.0);
+            double speedKmH = distance / totaltimeinhours;
+            double distanceinmiles = distance * 0.621371;
+            double speedMilesH = distanceinmiles / totaltimeinhours;
+            Console.WriteLine($"speed = {speedKmH} km/h");
+            Console.WriteLine($"speed = {speedMilesH} miles/h");
+        }
+        public static void Question05()
+        {
+            Console.Write("Enter a character: ");
+            char inputChar = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+            if ("aeiouAEIOU".Contains(inputChar))
+            {
+                Console.WriteLine($"{inputChar} is a vowel.");
+            }
+            else if (char.IsDigit(inputChar))
+            {
+                Console.WriteLine($"{inputChar} is a digit.");
+            }
+            else
+            {
+                Console.WriteLine($"{inputChar} is a symbol or consonant.");
+            }
+        }
+
+    }
+}
+
+
---
+++ CSDL-Exercises-LeDangNguyenThuy/Exercise-session2.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeDangNguyenThuy31231026554
+{
+    internal class Exercise_session2
+    {
+        public static void Main2()
+        {
+            //Question01();
+            //Question02();
+            //Question03();
+            Console.ReadKey();
+        }
+
+        public static void Question01()
+        {
+            Console.Write("Enter degrees Celsius: ");
+            double celsius = double.Parse(Console.ReadLine());
+            double kelvin = celsius + 273;
+            double fahrenheit = celsius * 18 / 10 + 32;
+            Console.WriteLine($"{celsius} celsius = {kelvin} kelvin");
+            Console.WriteLine($"{celsius} celsius = {fahrenheit} fahrenheit");
+
+        }
+        public static void Question02()
+        {
+            Console.Write("Enter radius: ");
+            double radius = double.Parse(Console.ReadLine());
+            double surface = 4 * Math.PI * Math.Pow(radius, 2); ;
+            double volume = 4 / 3 * Math.PI * Math.Pow(radius, 3);
+            Console.WriteLine($"surface= {surface}");
+            Console.WriteLine($"volume = {volume}");
+        }
+        public static void Question03()
+        {
+            Console.WriteLine("enter number 1:");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter number 2:");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            int sum = number1 + number2;
+            int subtract = number1 - number2;
+            int multiplying = number1 * number2;
+            int divide = number1 / number2;
+            int mod = number1 % number2;
+            Console.WriteLine($"{number1} + {number2} = {sum}");
+            Console.WriteLine($"{number1} - {number2} = {subtract}");
+            Console.WriteLine($"{number1} * {number2} = {multiplying}");
+            Console.WriteLine($"{number1} / {number2} = {divide}");
+            Console.WriteLine($"{number1} % {number2} = {mod}");
+
+        }
+
+    }
+}
+
+
+
